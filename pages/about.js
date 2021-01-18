4ddbf040dae6f052dca6f9a5b8ba4d37333a2a0c
jakdve047 <jakfreelancer047@gmail.com>
2021-01-18 22:32:29 +0600

    Static Images File Serving

--- pages/about.js
+++ pages/about.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Navigation from '../components/navigation';
+// import Image from 'next/image'
 
 const About = () => {
     return (
         <div>
             <Navigation />
            <h2>This is About page</h2> 
+           <img src="images/laptop.jpg" alt="laptop" width="400" height="400" />
         </div>
     )
 }
