9baaadf0898c3800ec03d80aa484757d5cb73888
jakdev047 <jakfreelancer047@gmail.com>
2021-07-13 18:02:43 +0600

    getServerSideProps

--- pages/contact.js
+++ pages/contact.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import Navigation from '../components/navigation';
 import Head from 'next/head';
+import axios from 'axios';
 
-const Contact = () => {
+export  const getServerSideProps  = async () => {
+    const res = await axios.get(`https://restcountries.eu/rest/v2/all`);
+    return {
+        props: {
+            countryArray: res?.data
+        }
+    };
+    // try {
+    //     const res = await axios.get(
+    //       `https://restcountries.eu/rest/v2/all`
+    //     );
+    //     if (res?.status === 200 && res?.data) {
+    //         // return {
+    //         //     props: {
+    //         //         countryArray: res?.data
+    //         //     }
+    //         // }
+    //     }
+    // } catch (error) {
+    //     console.log(error?.message);
+    // }
+}
+
+const Contact = ({countryArray}) => {
+    console.log('countryList',countryArray[13]);
     return (
         <div>
             <Head>
