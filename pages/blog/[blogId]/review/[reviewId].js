47d73a053648d6f2c9a0381e7aba2a8a1f2302e1
jakdev047 <jakfreelancer047@gmail.com>
2021-07-14 15:35:40 +0600

    Nested Dynamic Routes

--- pages/blog/[blogId]/review/[reviewId].js
+++ pages/blog/[blogId]/review/[reviewId].js
@@ -1,11 +1,11 @@
 import React from "react";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import Navigation from "../../components/navigation";
+import Navigation from "../../../../components/navigation";
 
-const SingleBlog = () => {
+const BlogReview = () => {
   const router = useRouter();
-  const blogId = router?.query?.blogId;
+  const { blogId, reviewId } = router?.query;
   return (
     <div>
       <Head>
@@ -13,9 +13,11 @@ const SingleBlog = () => {
         <title>Blog</title>
       </Head>
       <Navigation />
-      <h2>Blog {blogId}</h2>
+      <h2>
+        Review {reviewId} for Blog {blogId}
+      </h2>
     </div>
   );
 };
 
-export default SingleBlog;
+export default BlogReview;
