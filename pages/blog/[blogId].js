932f15044d9d41630350c53a3632431cc6923f06
jakdev047 <jakfreelancer047@gmail.com>
2021-07-14 15:27:22 +0600

    single blog

---
+++ pages/blog/[blogId].js
@@ -0,0 +1,21 @@
+import React from "react";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import Navigation from "../../components/navigation";
+
+const SingleBlog = () => {
+  const router = useRouter();
+  const blogId = router?.query?.blogId;
+  return (
+    <div>
+      <Head>
+        {/* title */}
+        <title>Blog</title>
+      </Head>
+      <Navigation />
+      <h2>Blog {blogId}</h2>
+    </div>
+  );
+};
+
+export default SingleBlog;
