13a6e6ddf952e9c466e4a712567b7bd0989306f6
jakdev047 <jakfreelancer047@gmail.com>
2021-07-14 12:42:46 +0600

    Working With Environment Variable

--- pages/contact.js
+++ pages/contact.js
@@ -1,43 +1,43 @@
-import React from 'react';
-import Navigation from '../components/navigation';
-import Head from 'next/head';
-import axios from 'axios';
+import React from "react";
+import Navigation from "../components/navigation";
+import Head from "next/head";
+import axios from "axios";
 
-export  const getServerSideProps  = async () => {
-    const res = await axios.get(`https://restcountries.eu/rest/v2/all`);
-    return {
-        props: {
-            countryArray: res?.data
-        }
-    };
-    // try {
-    //     const res = await axios.get(
-    //       `https://restcountries.eu/rest/v2/all`
-    //     );
-    //     if (res?.status === 200 && res?.data) {
-    //         // return {
-    //         //     props: {
-    //         //         countryArray: res?.data
-    //         //     }
-    //         // }
-    //     }
-    // } catch (error) {
-    //     console.log(error?.message);
-    // }
-}
+export const getServerSideProps = async () => {
+  const res = await axios.get(`https://restcountries.eu/rest/v2/all`);
+  return {
+    props: {
+      countryArray: res?.data,
+    },
+  };
+  // try {
+  //     const res = await axios.get(
+  //       `https://restcountries.eu/rest/v2/all`
+  //     );
+  //     if (res?.status === 200 && res?.data) {
+  //         // return {
+  //         //     props: {
+  //         //         countryArray: res?.data
+  //         //     }
+  //         // }
+  //     }
+  // } catch (error) {
+  //     console.log(error?.message);
+  // }
+};
 
-const Contact = ({countryArray}) => {
-    console.log('countryList',countryArray[13]);
-    return (
-        <div>
-            <Head>
-                {/* title */}
-                <title>Contact</title>
-            </Head>
-            <Navigation />
-           <h2>This is Contact page</h2> 
-        </div>
-    )
-}
+const Contact = ({ countryArray }) => {
+  console.log("countryList", countryArray[13]);
+  return (
+    <div>
+      <Head>
+        {/* title */}
+        <title>Contact</title>
+      </Head>
+      <Navigation />
+      <h2>This is Contact page</h2>
+    </div>
+  );
+};
 
 export default Contact;
--- pages/login.js
+++ pages/login.js
@@ -1,18 +1,27 @@
-import React from 'react';
-import Navigation from '../components/navigation';
-import Head from 'next/head';
+import React from "react";
+import Navigation from "../components/navigation";
+import Head from "next/head";
 
-const Login = () => {
-    return (
-        <div>
-            <Head>
-                {/* title */}
-                <title>Login</title>
-            </Head>
-            <Navigation />
-           <h2>This is Login page</h2> 
-        </div>
-    )
-}
+export const getStaticProps = async () => {
+  return {
+    props: {
+      BASEAPIURL: process.env.BASEAPIURL,
+    },
+  };
+};
+
+const Login = ({ BASEAPIURL }) => {
+  console.log("BASE_API_URL", BASEAPIURL);
+  return (
+    <div>
+      <Head>
+        {/* title */}
+        <title>Login</title>
+      </Head>
+      <Navigation />
+      <h2>This is Login page</h2>
+    </div>
+  );
+};
 
 export default Login;
